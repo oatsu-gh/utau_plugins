cdff8688d6d15bc2a089fa65007e31c3f739acbd
oatsu <panchi.psp@gmail.com>
2021-08-14 00:53:40 +0900

    メモ帳で開くプラグインを更新

--- open_with_notepad/open_with_notepad.py
+++ open_with_notepad/open_with_notepad.py
@@ -14,7 +14,6 @@ def main(path_exe, path_plugin_script):
     指定されたプログラムで指定されたファイルを開く。
     """
     print('メモ帳で編修中です。編集が終わったら、上書き保存してメモ帳を閉じてください。')
-    print(path_plugin_script)
     subprocess.run([path_exe, path_plugin_script], check=True)
 
 
